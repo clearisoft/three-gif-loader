8293ad6c43e704c03a8b4eae2a020c551fc23f4e
clearisoft-home <clearisoft@aliyun.com>
2021-12-17 18:06:00 +0800

    add play() and pause() add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+export default class GifLoader {
+  private manager: string
+  private crossOrigin: string
+  private path: string
+  
+  constructor(manager: any);
+  load(url: any, onLoad: any, onProgress: any, onError: any): any;
+  setPath(value: any): this;
+}
